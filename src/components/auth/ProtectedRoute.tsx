c7439b22c0e1c86e3676c4851a9d0c65fcb9edee
wyn-code <brunoo6.massocco@gmail.com>
2026-05-08 16:13:28 +0000

    se puede ir a la dashboard y cerrrar sesion

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,18 +1,74 @@
-import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
 import type { ReactNode } from "react";
 
-export default function ProtectedRoute({ children }: { children: ReactNode }) {
-  const { user, isLoading } = useAuth();
+import {
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+
+import { useAuth } from "../../contexts/AuthContext";
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+export default function ProtectedRoute({
+  children,
+}: ProtectedRouteProps) {
+  const {
+    isAuthenticated,
+    isLoading,
+  } = useAuth();
+
   const location = useLocation();
 
-  if (isLoading) return <p>Cargando...</p>;
+  // =========================
+  // LOADING
+  // =========================
 
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location.pathname }} replace />;
+  if (isLoading) {
+    return (
+      <div
+        className="
+          flex
+          min-h-screen
+          items-center
+          justify-center
+        "
+      >
+        <div
+          className="
+            h-10
+            w-10
+            animate-spin
+            rounded-full
+            border-2
+            border-primary
+            border-t-transparent
+          "
+        />
+      </div>
+    );
   }
 
-  console.log("PROTECTED ROUTE USER:", user);
+  // =========================
+  // NOT AUTHENTICATED
+  // =========================
+
+  if (!isAuthenticated) {
+    return (
+      <Navigate
+        to="/login"
+        replace
+        state={{
+          from: location.pathname,
+        }}
+      />
+    );
+  }
+
+  // =========================
+  // AUTHORIZED
+  // =========================
+
   return <>{children}</>;
-  
 }
